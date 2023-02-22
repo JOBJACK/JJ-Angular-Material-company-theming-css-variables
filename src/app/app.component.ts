f2b30d6bde84514d86534a98b74e4e218fc7d027
JOBJACK <119332782+JOBJACK@users.noreply.github.com>
2023-02-22 08:27:28 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,7 +68,7 @@ export class AppComponent {
   }
 
   savePrimaryColor() {
-    this.primaryColorPalette = computeColors(this.primaryColor);
+    this.primaryColorPalette = this.computeColors(this.primaryColor);
 
     for (const color of this.primaryColorPalette) {
       const key1 = `--theme-primary-${color.name}`;
@@ -81,7 +81,7 @@ export class AppComponent {
   }
 
   saveAccentColor() {
-    this.accentColorPalette = computeColors(this.accentColor);
+    this.accentColorPalette = this.computeColors(this.accentColor);
 
     for (const color of this.accentColorPalette) {
       const key1 = `--theme-accent-${color.name}`;
@@ -92,32 +92,32 @@ export class AppComponent {
       document.documentElement.style.setProperty(key2, value2);
     }
   }
-}
 
-function computeColors(hex: string): Color[] {
-  return [
-    getColorObject(tinycolor(hex).lighten(52), '50'),
-    getColorObject(tinycolor(hex).lighten(37), '100'),
-    getColorObject(tinycolor(hex).lighten(26), '200'),
-    getColorObject(tinycolor(hex).lighten(12), '300'),
-    getColorObject(tinycolor(hex).lighten(6), '400'),
-    getColorObject(tinycolor(hex), '500'),
-    getColorObject(tinycolor(hex).darken(6), '600'),
-    getColorObject(tinycolor(hex).darken(12), '700'),
-    getColorObject(tinycolor(hex).darken(18), '800'),
-    getColorObject(tinycolor(hex).darken(24), '900'),
-    getColorObject(tinycolor(hex).lighten(50).saturate(30), 'A100'),
-    getColorObject(tinycolor(hex).lighten(30).saturate(30), 'A200'),
-    getColorObject(tinycolor(hex).lighten(10).saturate(15), 'A400'),
-    getColorObject(tinycolor(hex).lighten(5).saturate(5), 'A700'),
-  ];
-}
+  computeColors(hex: string): Color[] {
+    return [
+      this.getColorObject(tinycolor(hex).lighten(52), '50'),
+      this.getColorObject(tinycolor(hex).lighten(37), '100'),
+      this.getColorObject(tinycolor(hex).lighten(26), '200'),
+      this.getColorObject(tinycolor(hex).lighten(12), '300'),
+      this.getColorObject(tinycolor(hex).lighten(6), '400'),
+      this.getColorObject(tinycolor(hex), '500'),
+      this.getColorObject(tinycolor(hex).darken(6), '600'),
+      this.getColorObject(tinycolor(hex).darken(12), '700'),
+      this.getColorObject(tinycolor(hex).darken(18), '800'),
+      this.getColorObject(tinycolor(hex).darken(24), '900'),
+      this.getColorObject(tinycolor(hex).lighten(50).saturate(30), 'A100'),
+      this.getColorObject(tinycolor(hex).lighten(30).saturate(30), 'A200'),
+      this.getColorObject(tinycolor(hex).lighten(10).saturate(15), 'A400'),
+      this.getColorObject(tinycolor(hex).lighten(5).saturate(5), 'A700'),
+    ];
+  }
 
-function getColorObject(value, name): Color {
-  const c = tinycolor(value);
-  return {
-    name: name,
-    hex: c.toHexString(),
-    darkContrast: c.isLight(),
-  };
+  getColorObject(value, name): Color {
+    const c = tinycolor(value);
+    return {
+      name: name,
+      hex: c.toHexString(),
+      darkContrast: c.isLight(),
+    };
+  }
 }
